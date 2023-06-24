de19a878a0a1f5138c30e76d367a615a770b8341
SNEHA CHAUHAN <sneha2003chauhan@gmail.com>
2023-06-24 12:14:13 +0530

    SignIn and SignUp page Set up

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Home from './Pages/Home';
 import './App.css';
 import { HashRouter, Routes, Route} from 'react-router-dom';
+import SignIn from 'Components/Auth/Signin';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <HashRouter>
         <Routes>
         <Route exact path="/" element={<Home />} />
+        <Route path="/signin" element={<SignIn />} />
         </Routes>
       </HashRouter>
   );
---
+++ client/src/Components/Auth/Signin.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SignIn = () => {
+  return (
+    <div>
+      <Signup />
+    </div>
+  )
+}
+
+export default SignIn;

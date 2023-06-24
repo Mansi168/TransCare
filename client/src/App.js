756df5ce958b556cd96e646c542a72075ecb4531
SNEHA CHAUHAN <sneha2003chauhan@gmail.com>
2023-06-24 13:53:16 +0000

    Error Resolved

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,29 @@
-
-import React from 'react'
+import React from 'react';
 import './App.css';
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AppProvider } from './context.';
 import Home from './Pages/Home';
 import { ChakraProvider } from '@chakra-ui/react';
 import Library from './Pages/library';
-
 import Navbar from './Components/LandingPage/Navbar';
 import BookDetails from "./Components/BookDetails/BookDetails";
 import BookList from "./Components/BookList/BookList";
 
 function App() {
   return (
-//     <ChakraProvider>
-    <AppProvider>
-    <HashRouter>
-      <Navbar/>
-   
-      <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path = "/library" element = {<Library />}></Route>
-      <Route path="/book" element={<BookList />} />
-     
-          <Route path = "/book/:id" element = {<BookDetails />} />
-     </Routes>
-   </HashRouter>
-   </AppProvider>
-// </ChakraProvider>
-
+    <ChakraProvider>
+      <AppProvider>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/library" element={<Library />} />
+            <Route path="/book" element={<BookList />} />
+            <Route path="/book/:id" element={<BookDetails />} />
+          </Routes>
+        </Router>
+      </AppProvider>
+    </ChakraProvider>
   );
 }
 

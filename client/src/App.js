1acb8edb70f94a4efa6c6c0460337171bb6f61eb
SNEHA CHAUHAN <sneha2003chauhan@gmail.com>
2023-06-24 12:04:00 +0530

    App Js Solved

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import Home from './Pages/Home';
 import './App.css';
+import { HashRouter, Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <>
-      <Home />
-    </>
+
+      <HashRouter>
+        <Routes>
+        <Route exact path="/" element={<Home />} />
+        </Routes>
+      </HashRouter>
   );
 }
 
